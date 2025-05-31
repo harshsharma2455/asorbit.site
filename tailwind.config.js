2853f1a4725a64d28f237903d51e893ae46cb2ac
Asorbit <harsh@asorbit.site>
2025-05-31 10:10:24 +0530

    new3

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx,vue}", // Adjust paths based on your project structure
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
